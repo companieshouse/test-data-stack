eccc340d4c65f734479df9fcd8f649e4aaf58591
ch-gareth <gedmunds@companieshouse.gov.uk>
2020-05-12 11:21:53 +0100

    Moves outputs to separate outputs file. DVOP-1197

--- groups/iam/module-iam-policies/main.tf
+++ groups/iam/module-iam-policies/main.tf
@@ -16,12 +16,3 @@ resource "aws_iam_policy" "iam_policy_destroy" {
   description = var.policy_description
   policy      = data.aws_iam_policy_document.test_data_generator_stack_terraform_destroy.json
 }
-
-# Outputs
-output "iam_policies_arns" {
-  value = {
-    "test-data-generator-stack-terraform-plan"    = aws_iam_policy.iam_policy_plan.arn
-    "test-data-generator-stack-terraform-apply"   = aws_iam_policy.iam_policy_apply.arn
-    "test-data-generator-stack-terraform-destroy" = aws_iam_policy.iam_policy_destroy.arn
-  }
-}
