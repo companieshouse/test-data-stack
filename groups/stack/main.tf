c6de3c5956ae163606a8e02696591ae57d425b0b
ch-gareth <gedmunds@companieshouse.gov.uk>
2020-07-22 15:14:43 +0100

    Hard-codes region for common networking state. DVOP-1360

--- groups/stack/main.tf
+++ groups/stack/main.tf
@@ -44,7 +44,7 @@ data "terraform_remote_state" "networks_common_infra_ireland" {
   backend = "s3"
   config = {
     bucket = "development-eu-west-1.terraform-state.ch.gov.uk"
-    key    = "aws-common-infrastructure-terraform/common-${var.aws_region}/networking.tfstate"
+    key    = "aws-common-infrastructure-terraform/common-eu-west-1/networking.tfstate"
     region = "eu-west-1"
   }
 }
