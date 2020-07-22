6d7b31fbc165a13ab530e44eb0225ff271e55a21
ch-gareth <gedmunds@companieshouse.gov.uk>
2020-07-22 14:20:08 +0100

    Cleans up alignment and white-spaces. DVOP-1360

--- groups/stack/main.tf
+++ groups/stack/main.tf
@@ -88,10 +88,9 @@ locals {
 module "ecs-cluster" {
   source = "git::git@github.com:companieshouse/terraform-library-ecs-cluster.git?ref=1.1.1"
 
-  stack_name  = local.stack_name
-  name_prefix = local.name_prefix
-  environment = var.environment
-
+  stack_name                 = local.stack_name
+  name_prefix                = local.name_prefix
+  environment                = var.environment
   vpc_id                     = local.vpc_id
   subnet_ids                 = local.application_ids
   ec2_key_pair_name          = var.ec2_key_pair_name
@@ -115,38 +114,37 @@ module "secrets" {
 module "ecs-stack" {
   source = "./module-ecs-stack"
 
-  stack_name  = local.stack_name
-  name_prefix = local.name_prefix
-  environment = var.environment
-
-  vpc_id                          = local.vpc_id
-  ssl_certificate_id              = var.ssl_certificate_id
-  zone_id                         = var.zone_id
-  external_top_level_domain       = var.external_top_level_domain
-  internal_top_level_domain       = var.internal_top_level_domain
   web_access_cidrs                = concat(local.internal_cidrs,local.vpn_cidrs,local.management_private_subnet_cidrs,split(",",local.application_cidrs))
-  subnet_ids                      = local.application_ids
+  stack_name                = local.stack_name
+  name_prefix               = local.name_prefix
+  environment               = var.environment
+  vpc_id                    = local.vpc_id
+  ssl_certificate_id        = var.ssl_certificate_id
+  zone_id                   = var.zone_id
+  external_top_level_domain = var.external_top_level_domain
+  internal_top_level_domain = var.internal_top_level_domain
+  subnet_ids                = local.application_ids
+}
 
 module "ecs-services" {
   source = "./module-ecs-services"
 
-  name_prefix = local.name_prefix
-  environment = var.environment
-
-  test-data-lb-arn = module.ecs-stack.test-data-lb-listener-arn
+  name_prefix               = local.name_prefix
+  environment               = var.environment
+  test-data-lb-arn          = module.ecs-stack.test-data-lb-listener-arn
   test-data-lb-listener-arn = module.ecs-stack.test-data-lb-listener-arn
-
-  vpc_id                          = local.vpc_id
-  aws_region                      = var.aws_region
-  ssl_certificate_id              = var.ssl_certificate_id
-  external_top_level_domain       = var.external_top_level_domain
-  internal_top_level_domain       = var.internal_top_level_domain
-  ecs_cluster_id                  = module.ecs-cluster.ecs_cluster_id
-  task_execution_role_arn         = module.ecs-cluster.ecs_task_execution_role_arn
-  docker_registry                 = var.docker_registry
-  release_version                 = var.release_version
-  application_port                = local.application_port
-
-  log_level       = var.log_level
-  secrets_arn_map = module.secrets.secrets_arn_map
+  vpc_id                    = local.vpc_id
+  aws_region                = var.aws_region
+  ssl_certificate_id        = var.ssl_certificate_id
+  external_top_level_domain = var.external_top_level_domain
+  internal_top_level_domain = var.internal_top_level_domain
+  ecs_cluster_id            = module.ecs-cluster.ecs_cluster_id
+  task_execution_role_arn   = module.ecs-cluster.ecs_task_execution_role_arn
+  docker_registry           = var.docker_registry
+  # service variables
+  # test-data-generator
+  release_version           = var.release_version
+  application_port          = "10000"
+  log_level                 = "DEBUG"
+  secrets_arn_map           = module.secrets.secrets_arn_map
 }
