f3ac282806fe28fbed3a2c5f26a343bccd95896e
ch-gareth <gedmunds@companieshouse.gov.uk>
2020-07-22 10:50:51 +0100

    Updates stack name to "test-data". DVOP-1360

--- groups/stack/main.tf
+++ groups/stack/main.tf
@@ -80,7 +80,7 @@ data "vault_generic_secret" "secrets" {
 
 locals {
   # stack name is hardcoded here in main.tf for this stack. It should not be overridden per env
-  stack_name       = "test-data-generator"
+  stack_name       = "test-data"
   name_prefix      = "${local.stack_name}-${var.environment}"
   application_port = "10000"
 }
