b17cef9e36b20df066f2a259ea774b0c70fedbe1
Wayne Jenkins <wjenkins1@companieshouse.gov.uk>
2024-03-19 10:35:21 +0000

    Update ecs-stack module to restructure security group rules to be less prone to race conditions, add rules for shared-services Concourse

--- groups/stack/module-ecs-stack/security-groups.tf
+++ groups/stack/module-ecs-stack/security-groups.tf
@@ -2,20 +2,6 @@ resource "aws_security_group" "internal-service-sg" {
   description = "Security group for internal service albs"
   vpc_id      = var.vpc_id
 
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = var.web_access_cidrs
-  }
-
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = var.web_access_cidrs
-  }
-
   egress {
     from_port   = 0
     to_port     = 0
@@ -27,4 +13,48 @@ resource "aws_security_group" "internal-service-sg" {
     Environment = var.environment
     Name        = "${var.name_prefix}-internal-service-sg"
   }
+}
+
+resource "aws_security_group_rule" "web_http" {
+  for_each = toset(var.web_access_cidrs)
+
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  cidr_blocks       = [each.value]
+  security_group_id = aws_security_group.internal-service-sg.id
+}
+
+resource "aws_security_group_rule" "web_https" {
+  for_each = toset(var.web_access_cidrs)
+
+  type              = "ingress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  cidr_blocks       = [each.value]
+  security_group_id = aws_security_group.internal-service-sg.id
+}
+
+resource "aws_security_group_rule" "concourse_http" {
+  for_each = toset(var.concourse_access_cidrs)
+
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  cidr_blocks       = [each.value]
+  security_group_id = aws_security_group.internal-service-sg.id
+}
+
+resource "aws_security_group_rule" "concourse_https" {
+  for_each = toset(var.concourse_access_cidrs)
+
+  type              = "ingress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  cidr_blocks       = [each.value]
+  security_group_id = aws_security_group.internal-service-sg.id
 }
