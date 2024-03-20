e3123c0c358b3f90d214e6d586b2616f5499ac9e
Wayne Jenkins <wjenkins1@companieshouse.gov.uk>
2024-03-20 07:56:59 +0000

    Ensure local CIDRs can access via the LB

--- groups/stack/locals.tf
+++ groups/stack/locals.tf
@@ -16,7 +16,8 @@ locals {
 
   public_lb_cidrs  = ["0.0.0.0/0"]
   lb_subnet_ids    = "${var.test_data_lb_internal ? local.application_ids : local.public_ids}" # place ALB in correct subnets
-  lb_access_cidrs  = "${var.test_data_lb_internal ? concat(local.management_private_subnet_cidrs,split(",",local.application_cidrs)) : local.public_lb_cidrs }"
+#  lb_access_cidrs  = "${var.test_data_lb_internal ? concat(local.management_private_subnet_cidrs,split(",",local.application_cidrs)) : local.public_lb_cidrs }"
+  lb_access_cidrs  = "${var.test_data_lb_internal ? concat(local.internal_cidrs,local.vpn_cidrs,local.management_private_subnet_cidrs,split(",",local.application_cidrs)) : local.public_lb_cidrs }"
   app_access_cidrs = "${var.test_data_lb_internal ? concat(local.internal_cidrs,local.vpn_cidrs,local.management_private_subnet_cidrs,split(",",local.application_cidrs)) : concat(local.internal_cidrs,local.vpn_cidrs,local.management_private_subnet_cidrs,split(",",local.application_cidrs),split(",",local.public_cidrs)) }"
 
   concourse_access_cidrs = values(data.vault_generic_secret.shared_services_concourse_cidrs.data)
